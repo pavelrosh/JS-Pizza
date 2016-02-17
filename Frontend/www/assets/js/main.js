4d498a40f8608ebbcc3866fff68a313737da9e22
pavelrosh <pavelrosh@gmail.com>
2016-02-17 09:54:13 +0200

    pizza_add_to_rightpanel

--- Frontend/www/assets/js/main.js
+++ Frontend/www/assets/js/main.js
@@ -7,3 +7,26 @@ $(function(){
         return false;
     });
 });
+$(document).ready(function(){
+
+    var LIST = $(".pizzabar");    //кошик для піци
+    var name_pizza;
+    var price_pizza;
+    var PIZZA_TEMPLATE=$('.new_pizza_small').html(); //шаблон замовлено піци small
+
+    $('.small').click(function(){
+        name_pizza= $(this).find('.caption').text();
+        price_pizza=$(this).parent().find('.smpizza_price').text();
+        addToCart(name_pizza,price_pizza);
+    });
+
+    function addToCart(name_pizza,price_pizza) {
+        var node = $(PIZZA_TEMPLATE); //сворив новий елемет з піцою
+        node.find('.nazva').text(name_pizza);
+        node.find('.price').text(price_pizza);
+        LIST.append(node);
+
+        updateCart();
+    }
+  //  addToCart("Piccolo","150грн");
+});
